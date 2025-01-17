211c4b3d453eb19a574e5b4f1f7b299da70ccf98
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-01-17 15:47:01 +0200

    After Reset

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import { fileURLToPath } from "url";
+import path from "path";
 
 dotenv.config();
+const __dirname = path.dirname(fileURLToPath(import.meta.url)); // Correct way to get __dirname in ES modules
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,7 +25,12 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const PORT = process.env.PORT;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist"))); // Use __dirname to get the correct path
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is Running on PORT ${PORT}`);
