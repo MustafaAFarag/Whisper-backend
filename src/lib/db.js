d3ff276f28537e7b3d4f8d8aabd68a501a95c1c2
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-01-17 11:13:31 +0200

    Static Code Removed

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,9 +2,14 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log(`MongoDB Connected : ${conn.connection.host}`);
   } catch (error) {
     console.log(`MongoDB connection Error ${error}`);
+    process.exit(1);
   }
 };
